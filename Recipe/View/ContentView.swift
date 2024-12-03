eb1f9614fc7d3a4c76d13d8173afbb3a56bfa241
allisonwinder <allisonwinder1@gmail.com>
2024-12-03 10:47:54 -0700

    It looks nice but it isn't a format I would like

--- Recipe/View/ContentView.swift
+++ Recipe/View/ContentView.swift
@@ -23,11 +23,77 @@ struct ContentView: View {
                         // Filter the recipes by the selected category
                         List(category.recipes) { recipe in
                             NavigationLink {
-                                RecipeDetailView(recipe: recipe)
+                                VStack(alignment: .leading, spacing: 20) {
+                                    // Recipe Name
+                                    Markdown("# \(recipe.name)")
+                                        .font(.largeTitle)
+                                        .padding(.bottom, 10)
+                                    
+                                    // Recipe Metadata
+                                    HStack(spacing: 10) {
+                                        Text("Author: \(recipe.author)")
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
+                                        Text("Servings: \(recipe.servings)")
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
+                                        Text("Time: \(recipe.time) min")
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
+                                        Text("Date Added: \(recipe.dateAdded.formatted())")
+                                            .font(.subheadline)
+                                            .foregroundStyle(.secondary)
+                                        Spacer()
+                                        // Favorite Toggle
+                                        Button(action: {
+                                            recipe.favorite.toggle() // Toggle favorite status
+                                        }) {
+                                            Image(systemName: recipe.favorite ? "heart.fill" : "heart")
+                                                .foregroundColor(recipe.favorite ? .red : .gray)
+                                                .imageScale(.large)
+                                        }
+                                        .buttonStyle(.borderless)
+                                    }
+                                    .padding(.bottom, 10)
+                                    
+                                    // Categories as Chips
+                                    if !recipe.categories.isEmpty {
+                                        ScrollView(.horizontal, showsIndicators: false) {
+                                            HStack(spacing: 10) {
+                                                ForEach(recipe.categories) { category in
+                                                    Text(category.name)
+                                                        .font(.caption)
+                                                        .padding(.horizontal, 10)
+                                                        .padding(.vertical, 5)
+                                                        .background(Capsule().fill(.blue.opacity(0.2)))
+                                                        .foregroundColor(.blue)
+                                                }
+                                            }
+                                            .padding(.bottom, 10)
+                                        }
+                                    }
+                                    
+                                    // Ingredients Section
+                                    Markdown("### Ingredients")
+                                    Markdown(recipe.ingredients) // Directly display the ingredients string
+                                        .padding(.bottom, 10)
+                                    
+                                    // Instructions Section
+                                    Markdown("### Instructions")
+                                    Markdown(recipe.instructions) // Directly display the instructions string
+                                    
+                                    // Edit Button
+                                    Button("Edit Recipe") {
+                                        recipeToEdit = recipe
+                                    }
+                                    .buttonStyle(.bordered)
+                                    .padding(.top)
+                                }
+                                .padding()
+                                .markdownTheme(.gitHub)
                             } label: {
                                 Text(recipe.name)
                             }
-
                         }
                     } label: {
                         Text(category.name)
@@ -47,7 +113,75 @@ struct ContentView: View {
             List {
                 ForEach(viewModel.allRecipes) { recipe in
                     NavigationLink {
-                        RecipeDetailView(recipe: recipe)
+                        //RecipeDetailView(recipe: recipe)
+                        VStack(alignment: .leading, spacing: 20) {
+                            // Recipe Name
+                            Markdown("# \(recipe.name)")
+                                .font(.largeTitle)
+                                .padding(.bottom, 10)
+                            
+                            // Recipe Metadata
+                            HStack(spacing: 10) {
+                                Text("Author: \(recipe.author)")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                Text("Servings: \(recipe.servings)")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                Text("Time: \(recipe.time) min")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                        Text("Date Added: \(recipe.dateAdded.formatted())")
+                                            .font(.subheadline)
+                                            .foregroundStyle(.secondary)
+                                Spacer()
+                                // Favorite Toggle
+                                Button(action: {
+                                    recipe.favorite.toggle() // Toggle favorite status
+                                }) {
+                                    Image(systemName: recipe.favorite ? "heart.fill" : "heart")
+                                        .foregroundColor(recipe.favorite ? .red : .gray)
+                                        .imageScale(.large)
+                                }
+                                .buttonStyle(.borderless)
+                            }
+                            .padding(.bottom, 10)
+                            
+                            // Categories as Chips
+                            if !recipe.categories.isEmpty {
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack(spacing: 10) {
+                                        ForEach(recipe.categories) { category in
+                                            Text(category.name)
+                                                .font(.caption)
+                                                .padding(.horizontal, 10)
+                                                .padding(.vertical, 5)
+                                                .background(Capsule().fill(.blue.opacity(0.2)))
+                                                .foregroundColor(.blue)
+                                        }
+                                    }
+                                    .padding(.bottom, 10)
+                                }
+                            }
+                            
+                            // Ingredients Section
+                            Markdown("### Ingredients")
+                            Markdown(recipe.ingredients) // Directly display the ingredients string
+                                .padding(.bottom, 10)
+                            
+                            // Instructions Section
+                            Markdown("### Instructions")
+                            Markdown(recipe.instructions) // Directly display the instructions string
+                            
+                            // Edit Button
+                            Button("Edit Recipe") {
+                                recipeToEdit = recipe
+                            }
+                            .buttonStyle(.bordered)
+                            .padding(.top)
+                        }
+                        .padding()
+                        .markdownTheme(.gitHub)
                     } label: {
                         Text(recipe.name)
                     }
